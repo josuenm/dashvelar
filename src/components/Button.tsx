3d3c01c8c7bb1d9dde13ea2c17fbf38ff92b972e
josuenm <direct.josue@gmail.com>
2022-09-20 17:53:46 -0300

    feat: interface, button styled

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,21 @@
-const Button = () => {
-  return <button>Button</button>;
+interface ButtonProps {
+  color: string;
+  bgColor: string;
+  title: string;
+  borderRadius: string;
+  size?: string;
+}
+
+const Button = ({ title, bgColor, color, borderRadius, size }: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text-${size} p-3 duration-100 hover:drop-shadow-xl`}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default Button;
