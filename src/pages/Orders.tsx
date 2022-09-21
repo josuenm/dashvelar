07ebc20bdc044e53720c0c91b1d603dbf8e91c8d
josuenm <direct.josue@gmail.com>
2022-09-21 10:20:03 -0300

    feat: components are functional, components are styled

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,5 +1,52 @@
+import { ordersData, ordersGrid } from "@assets/data/dummy";
+import { Header } from "@components/index";
+import { Inject } from "@syncfusion/ej2-react-charts";
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  ContextMenu,
+  Edit,
+  ExcelExport,
+  Filter,
+  GridComponent,
+  Page,
+  PdfExport,
+  Resize,
+  Sort,
+} from "@syncfusion/ej2-react-grids";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <main className="my-12 p-2 md:p-10 lg:max-w-5xl mx-auto bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </main>
+  );
 };
 
 export default Orders;
