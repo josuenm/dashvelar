92301b3959e3c401037005c71dc84332cacf3514
josuenm <direct.josue@gmail.com>
2022-09-21 09:38:47 -0300

    feat: header configurated and styled
    Header receiving parameters, typed parameters, elements placed on screen, stylized elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
-const Header = () => {
-  return <header>Header</header>;
+interface HeaderProps {
+  category: string;
+  title: string;
+}
+
+const Header = ({ category, title }: HeaderProps) => {
+  return (
+    <header className="mb-10">
+      <p className="text-gray-400">{category}</p>
+      <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+        {title}
+      </p>
+    </header>
+  );
 };
 
 export default Header;
