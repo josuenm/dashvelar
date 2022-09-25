216addaf965e3f860d26f08765815ad5d8b3f78e
josuenm <direct.josue@gmail.com>
2022-09-25 14:35:35 -0300

    feat: components are functional and styled

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,5 +1,30 @@
+import {kanbanData, kanbanGrid} from "@assets/data/dummy";
+import { Header } from "@components/index";
+import {
+    KanbanComponent,
+    ColumnsDirective,
+    ColumnDirective
+} from "@syncfusion/ej2-react-kanban";
+
+
 const Kanban = () => {
-  return <div>Kanban</div>;
+    return (
+        <div className="m-2 md-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="App" title="Kanban" />
+            <KanbanComponent 
+                id="kanban"
+                dataSource={kanbanData}
+                cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+                keyField="Status"
+            >
+                <ColumnsDirective>
+                    {kanbanGrid.map((item, index) => (
+                        <ColumnDirective key={index} {...item} />
+                    ))}
+                </ColumnsDirective>
+            </KanbanComponent>
+        </div>
+    )
 };
 
 export default Kanban;
