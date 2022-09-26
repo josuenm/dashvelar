7824694437cc773a012069a2a8235784ef546181
josuenm <direct.josue@gmail.com>
2022-09-26 17:08:25 -0300

    feat: components are functional and stylized

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,5 +1,16 @@
+import {EditorData} from "@assets/data/dummy";
+import { Header } from "@components/index";
+import { RichTextEditorComponent, HtmlEditor, Inject, Toolbar, Link, QuickToolbar } from "@syncfusion/ej2-react-richtexteditor";
+
 const Editor = () => {
-  return <div>Editor</div>;
+    return (
+        <div className="m-3 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="App" title="Kanban" />
+            <RichTextEditorComponent>
+                <Inject services={[HtmlEditor, Toolbar, Link, QuickToolbar]} />
+            </RichTextEditorComponent>
+        </div>
+    );
 };
 
 export default Editor;
