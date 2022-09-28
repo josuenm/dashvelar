8589367069af0a6a8396a2d51af3416b64506eeb
josuenm <direct.josue@gmail.com>
2022-09-28 14:39:39 -0300

    feat: pages are functional

--- src/pages/Charts/Area.tsx
+++ src/pages/Charts/Area.tsx
@@ -1,5 +1,49 @@
+import {
+  areaCustomSeries,
+  AreaPrimaryXAxis,
+  AreaPrimaryYAxis,
+} from "@assets/data/dummy";
+import { Header } from "@components/index";
+import { useStateContext } from "@contexts/ContextProvider";
+import {
+  ChartComponent,
+  DateTime,
+  Inject,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  SplineAreaSeries,
+  ValueType,
+} from "@syncfusion/ej2-react-charts";
+
 const Area = () => {
-  return <div>Area</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <main className="min-h-screen">
+      <div className="lg:max-w-5xl mt-24 mx-auto p-10 bg-white rounded-3xl">
+        <Header category="Area" title="Inflation Rate in Percentage" />
+        <div className="w-full">
+          <ChartComponent
+            id="line-chart"
+            height="420px"
+            primaryXAxis={AreaPrimaryXAxis as { valueType: ValueType }}
+            primaryYAxis={AreaPrimaryYAxis}
+            chartArea={{ border: { width: 0 } }}
+            tooltip={{ enable: true }}
+            background={currentMode === "Dark" ? "#fff" : "#fff"}
+          >
+            <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+            <SeriesCollectionDirective>
+              {areaCustomSeries.map((item, index) => (
+                <SeriesDirective key={index} {...item} />
+              ))}
+            </SeriesCollectionDirective>
+          </ChartComponent>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Area;
--- src/pages/Charts/Line.tsx
+++ src/pages/Charts/Line.tsx
@@ -1,5 +1,16 @@
+import { Header, LineChart } from "@components/index";
+
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <main className="min-h-screen">
+      <div className="lg:max-w-5xl mt-24 mx-auto p-10 bg-white rounded-3xl">
+        <Header category="Chart" title="Inflation Rate" />
+        <div className="w-full">
+          <LineChart />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Line;
