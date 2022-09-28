1a384f5829058522c236ef169084b32aba4c5bae
josuenm <direct.josue@gmail.com>
2022-09-28 14:40:24 -0300

    feat: chart are functional

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -1,5 +1,43 @@
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "@assets/data/dummy";
+import { useStateContext } from "@contexts/ContextProvider";
+import {
+  ChartComponent,
+  ChartRangePadding,
+  DateTime,
+  Inject,
+  Legend,
+  LineSeries,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Tooltip,
+  ValueType,
+} from "@syncfusion/ej2-react-charts";
+
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis as { valueType: ValueType }}
+      primaryYAxis={LinePrimaryYAxis as { rangePadding: ChartRangePadding }}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#fff" : "#fff"}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
